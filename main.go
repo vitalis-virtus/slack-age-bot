42db29d0f296d845fbe5eab0b3d9783216cdf101
vitalis-virtus <vshayuk@gmail.com>
2022-09-28 15:12:16 +0300

    fix imports and add transfer tockens to .env

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
 	"log"
 	"os"
@@ -14,7 +15,7 @@ import (
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
 		fmt.Println("Command Events")
-		fmt.Println(event.TimeStamp)
+		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
 		fmt.Println(event.Parameters)
 		fmt.Println(event.Event)
@@ -23,8 +24,11 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A043N1W504X-4138636833410-b7af698500e423d90ca47664a5fbd45c50a55516bcf8c4b0b94c8a30e468cade")
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4124055123623-4138664024402-IeuiBGlxt7c38kcuesmLwqq9")
+
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
@@ -32,7 +36,7 @@ func main() {
 
 	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
-		Example:     "my yob is 2020",
+		Examples:    []string{"my yob is 2020"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
@@ -42,7 +46,6 @@ func main() {
 			age := 2022 - yob
 			r := fmt.Sprintf("age is %d", age)
 			response.Reply(r)
-
 		},
 	})
 
